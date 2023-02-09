85362a1452193c63fdc5f3867ce96ae779245e0e
danielmshawn <danielmshawn@gmail.com>
2023-02-08 16:03:43 -0800

    Add detail and add destination functionality

--- models/flight.js
+++ models/flight.js
@@ -2,11 +2,33 @@ const mongoose = require('mongoose');
 //Shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    destination: {
+        airport: { type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN', 'ORD']   
+    },
+        arrival: Date
+ }}, {
+    timestamps: true
+});
+
 const flightSchema = new Schema({
-    airline: String,
-    airport: String,
-    flightNo: Number,
-    departs: Date
+    airline: { type: String, 
+        enum:['American', 'Southwest', 'United', 'Alaskan', 'Jet Blue'] 
+            },
+    airport: {type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN', 'ORD'] },
+    flightNo: {type: Number, min: 10, max: 9999},
+    departs: {
+        type: Date, 
+        default: function() {
+            return new Date().getFullYear()+1;
+        }
+    },
+    destinations: [destinationSchema]
 }, {
     timestamps: true
 });
